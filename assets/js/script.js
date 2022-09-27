3302b7320d460fa55cb261f2e306b313f9c07001
Simone Codarin <codarinsimone@gmail.com>
2022-09-27 17:54:56 +0200

    Modificato html e js. Creazione tutto tramite js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,45 @@
-var count = 0;
+document.addEventListener('DOMContentLoaded', function( ){
+    var a = 0;
 
-function aggiungi(){
-    count++;
-    document.getElementById("numero").innerHTML = count; 
-}
-function sottrai(){
-    if(count != 0){
-        count--;
-        document.getElementById("numero").innerHTML = count;
-    }
-}
-function reset(){
-    count = 0;
-    document.getElementById("numero").innerHTML = count;
-}
+    const contatore = document.querySelector(".counter");
+    const title = document.createElement("h2");
+    title.textContent = "0";
+    contatore.appendChild(title);
+
+    const aggiungi = document.querySelector(".counter");
+    
+    const minusOne = document.createElement("botton");
+    minusOne.textContent = "-";
+    aggiungi.appendChild(minusOne);
+
+    const plusOne = document.createElement("botton");
+    plusOne.textContent = "+";
+    aggiungi.appendChild(plusOne);
+    
+    const reset = document.createElement("botton");
+    reset.textContent = "reset";
+    aggiungi.appendChild(reset);
+    
+
+    plusOne.setAttribute("class", "bottoni");
+    plusOne.setAttribute("id", "piu");
+
+    minusOne.setAttribute("class", "bottoni");
+
+    reset.setAttribute("class", "reset");
+
+    plusOne.addEventListener("click", function(){
+        a++;
+        document.querySelector("h2").innerHTML = a;
+    })
+    minusOne.addEventListener("click", function(){
+        if (a>0){
+            a--;
+            document.querySelector("h2").innerHTML = a;
+        }
+    })
+    reset.addEventListener("click", function(){
+        a = 0;
+        document.querySelector("h2").innerHTML = a;
+    })
+});

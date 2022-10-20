2d216f4d8baadad34e468da1721cd5d147fe9d17
Simone Codarin <codarinsimone@gmail.com>
2022-10-20 19:12:37 +0200

    aggiornamento function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,31 +3,29 @@ document.addEventListener('DOMContentLoaded', function( ){
     function contenuto(){
         var a = 0;
 
-        const contatore = document.querySelector(".counter");
-        const title = document.createElement("h2");
-        title.textContent = "0";
-        contatore.appendChild(title);
+        function createElement(){
 
-        const minusOne = document.createElement("botton");
-        minusOne.textContent = "-";
-        contatore.appendChild(minusOne);
+            const contatore = document.querySelector('.counter');
+            const title = document.createElement('h2');
+            title.textContent = '0';
+            contatore.appendChild(title);
 
-        const plusOne = document.createElement("botton");
-        plusOne.textContent = "+";
-        contatore.appendChild(plusOne);
-    
-        const reset = document.createElement("botton");
-        reset.textContent = "reset";
-        contatore.appendChild(reset);
-    
-        plusOne.setAttribute("class", "bottoni");
-        plusOne.setAttribute("id", "piu");
-
-        minusOne.setAttribute("class", "bottoni");
-        minusOne.setAttribute("id", "meno");
-
-        reset.setAttribute("class", "reset");
-        reset.setAttribute("id", "res");
+            var text = ['-', '+', 'RESET'];
+            for(var i = 0; i < text.length; i ++){
+                var button = document.createElement('button');
+                button.innerHTML = text[i];
+                contatore.appendChild(button);
+            };
+            var classSet = document.querySelectorAll('button');
+            for(var x=0; x<classSet.length-1; x++){
+	            if (classSet[x].setAttribute('class', 'bottoni'));
+            };
+            var id = ['meno', 'piu', 'res'];
+            for(var y=0; y<id.length; y++){
+	            if (classSet[y].setAttribute('id', id[y]));
+            };
+        }
+        createElement();
 
         document.querySelector(".counter").addEventListener("click", (e) => {
             if(e.target.id === "piu"){

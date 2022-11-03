8db0aada360c7476fb398bd792b2c06c4c2ec5a3
Simone Codarin <codarinsimone@gmail.com>
2022-11-03 18:03:29 +0100

    Modifica creazione pulsanti con un unica funzione

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,34 @@
-document.addEventListener('DOMContentLoaded', function( ){
+document.addEventListener('DOMContentLoaded', function(){
 
-    function contenuto(){
-        var a = 0;
+        let counter = 0;
 
-        function createElement(){
+        const counterBox = document.querySelector('.counterBox');
+        
+        const createCustomElement = (type, text, className, id) => {
+            let e = document.createElement(type);
+            e.textContent = text;
+            e.setAttribute('class', className);
+            e.setAttribute('id', id)
+            counterBox.appendChild(e);
+        };
 
-            const contatore = document.querySelector('.counter');
-            const title = document.createElement('h2');
-            title.textContent = '0';
-            contatore.appendChild(title);
+        createCustomElement('h2','0','null','null');
+        createCustomElement('button','+','bottoni','piu');
+        createCustomElement('button','-','bottoni','meno');
+        createCustomElement('button','reset', 'reset', 'res');
 
-            var text = ['-', '+', 'RESET'];
-            for(var i = 0; i < text.length; i ++){
-                var button = document.createElement('button');
-                button.innerHTML = text[i];
-                contatore.appendChild(button);
-            };
-            var classSet = document.querySelectorAll('button');
-            for(var x=0; x<classSet.length-1; x++){
-	            if (classSet[x].setAttribute('class', 'bottoni'));
-            };
-            var id = ['meno', 'piu', 'res'];
-            for(var y=0; y<id.length; y++){
-	            if (classSet[y].setAttribute('id', id[y]));
-            };
-        }
-        createElement();
-
-        document.querySelector(".counter").addEventListener("click", (e) => {
+        counterBox.addEventListener("click", (e) => {
             if(e.target.id === "piu"){
-                a++;
-                document.querySelector("h2").innerHTML = a;
+                counter++;
+                document.querySelector("h2").innerHTML = counter;
             } else if (e.target.id === "meno"){
-                if(a>0){
-                    a--;
-                    document.querySelector("h2").innerHTML = a;
+                if(counter>0){
+                    counter--;
+                    document.querySelector("h2").innerHTML = counter;
                 }
             } else if (e.target.id === "res"){
-                a = 0;
-                document.querySelector("h2").innerHTML = a;
+                counter = 0;
+                document.querySelector("h2").innerHTML = counter;
             }
         })
-    };
-
-    contenuto();
 });
